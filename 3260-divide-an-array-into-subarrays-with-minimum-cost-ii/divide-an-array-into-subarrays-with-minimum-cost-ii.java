0ad0477f606ce16d9b0367b0bd187856673c20b0
Aditya pal <adityapal622820@gmail.com>
2026-02-02 07:24:13 +0530

    Time: 653 ms (8.70%) | Memory: 94.3 MB (43.48%) - LeetSync

---
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.java
@@ -0,0 +1,108 @@
+class Solution {
+
+    TreeMap<Integer, Integer> small = new TreeMap<>();
+    TreeMap<Integer, Integer> large = new TreeMap<>();
+
+    long sumSmall = 0;
+    int sizeSmall = 0;
+    int sizeLarge = 0;
+    int need;
+
+    public long minimumCost(int[] nums, int k, int dist) {
+        int n = nums.length;
+        need = k - 1;
+
+        long base = nums[0];
+
+        // Initial window [1 .. dist + 1]
+        for (int i = 1; i <= dist + 1; i++) {
+            add(large, nums[i]);
+            sizeLarge++;
+        }
+
+        // Move smallest k-1 into small
+        while (sizeSmall < need) {
+            int x = large.firstKey();
+            remove(large, x);
+            sizeLarge--;
+            add(small, x);
+            sizeSmall++;
+            sumSmall += x;
+        }
+
+        long ans = sumSmall;
+
+        // Slide window
+        for (int i = dist + 2; i < n; i++) {
+            int out = nums[i - (dist + 1)];
+            int in = nums[i];
+
+            // Remove outgoing
+            if (small.containsKey(out)) {
+                remove(small, out);
+                sizeSmall--;
+                sumSmall -= out;
+            } else {
+                remove(large, out);
+                sizeLarge--;
+            }
+
+            // Add incoming
+            add(large, in);
+            sizeLarge++;
+
+            // Fix ordering if needed
+            if (sizeSmall > 0 && in < small.lastKey()) {
+                int x = small.lastKey();
+                remove(small, x);
+                sizeSmall--;
+                sumSmall -= x;
+
+                add(large, x);
+                sizeLarge++;
+
+                remove(large, in);
+                sizeLarge--;
+                add(small, in);
+                sizeSmall++;
+                sumSmall += in;
+            }
+
+            rebalance();
+            ans = Math.min(ans, sumSmall);
+        }
+
+        return base + ans;
+    }
+
+    private void rebalance() {
+        while (sizeSmall > need) {
+            int x = small.lastKey();
+            remove(small, x);
+            sizeSmall--;
+            sumSmall -= x;
+
+            add(large, x);
+            sizeLarge++;
+        }
+
+        while (sizeSmall < need) {
+            int x = large.firstKey();
+            remove(large, x);
+            sizeLarge--;
+
+            add(small, x);
+            sizeSmall++;
+            sumSmall += x;
+        }
+    }
+
+    private void add(TreeMap<Integer, Integer> map, int x) {
+        map.put(x, map.getOrDefault(x, 0) + 1);
+    }
+
+    private void remove(TreeMap<Integer, Integer> map, int x) {
+        map.put(x, map.get(x) - 1);
+        if (map.get(x) == 0) map.remove(x);
+    }
+}
