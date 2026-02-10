59c3942f2d14a40c4f2c9b8f90a6258a52a7a2f5
Aditya pal <adityapal622820@gmail.com>
2026-02-11 01:10:49 +0530

    Time: 3 ms (30.24%) | Memory: 45.7 MB (77.44%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,17 +1,26 @@
 class Solution {
-    public TreeNode helper(int[] preorder,int prelo, int prehi, int[] inorder, int inlo, int inhi){
-        if(prelo > prehi) return null;
-        TreeNode root = new TreeNode(preorder[prelo]);
-        int i = inlo;
-        while(inorder[i] != preorder[prelo]) i++;
-        int leftsize = i-inlo;
-        root.left = helper(preorder,prelo+1,prelo+leftsize,inorder,inlo,i-1);
-        root.right = helper(preorder,prelo+leftsize+1,prehi,inorder,i+1,inhi);
-        return root;
+    int preIndex = 0;
 
-    }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        return helper(preorder,0,n-1,inorder,0,n-1);
+        return build(preorder, inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int[] inorder, int inStart, int inEnd) {
+        if (inStart > inEnd) return null;
+
+        // Root from preorder
+        TreeNode root = new TreeNode(preorder[preIndex++]);
+
+        // Find root in inorder (linear search)
+        int mid = inStart;
+        while (inorder[mid] != root.val) {
+            mid++;
+        }
+
+        // Build left and right subtrees
+        root.left = build(preorder, inorder, inStart, mid - 1);
+        root.right = build(preorder, inorder, mid + 1, inEnd);
+
+        return root;
     }
 }
