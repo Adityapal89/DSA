4b212097fcae7beb78085b84311519a104a7e371
Aditya pal <adityapal622820@gmail.com>
2026-04-14 21:57:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (96.35%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -7,11 +7,11 @@ class Solution {
             int pos = t.charAt(n-1) - 48;
             arr[pos-1] = t.substring(0,n-1);
         }
-        StringBuilder sb = new StringBuilder();
-        for(String t : arr){
-            sb.append(t);
-            sb.append(" ");
-        }
-        return sb.toString().trim();
+        // StringBuilder sb = new StringBuilder();
+        // for(String t : arr){
+        //     sb.append(t);
+        //     sb.append(" ");
+        // }
+        return String.join(" ",arr);
     }
 }
