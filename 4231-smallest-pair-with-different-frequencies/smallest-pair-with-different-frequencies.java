30bd4abc12fddbf4cdb18a21c6c1505aea298c9e
Aditya pal <adityapal622820@gmail.com>
2026-04-27 17:44:38 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (97.32%) - LeetSync

---
+++ 4231-smallest-pair-with-different-frequencies/smallest-pair-with-different-frequencies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] minDistinctFreqPair(int[] nums) {
+        int[] freq = new int[101];
+        for (int i = 0; i < nums.length; i++) {
+            freq[nums[i]]++;
+        }
+        for (int x = 1; x <= 100; x++) {
+            if (freq[x] == 0) continue;
+            for (int y = x + 1; y <= 100; y++) {
+                if (freq[y] == 0) continue;
+                if (freq[x] != freq[y]) {
+                    return new int[]{x, y};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
