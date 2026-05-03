178c15a1b8b73d36df75d9d6d5448b67f7cb3a38
Aditya pal <adityapal622820@gmail.com>
2026-05-03 12:07:07 +0530

    Time: 2 ms (100.00%) | Memory: 46.3 MB (97.65%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.java
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
-        // int[] freq = new int[101];
-        // for (int b : bulbs) {
-        //     freq[b]++;
-        // }
-        // List<Integer> result = new ArrayList<>();
-        // for (int i = 1; i <= 100; i++) {
-        //     if (freq[i] % 2 == 1) {
-        //         result.add(i);
-        //     }
-        // }
-        // return result;
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<bulbs.size(); i++){
-            if(set.contains(bulbs.get(i))){
-                set.remove(bulbs.get(i));
-            } else{
-                set.add(bulbs.get(i));
+        int[] freq = new int[101];
+        for (int b : bulbs) {
+            freq[b]++;
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            if (freq[i] % 2 == 1) {
+                result.add(i);
             }
         }
-        List<Integer> li = new ArrayList<>(set);
-        Collections.sort(li);
-        return li;
+        return result;
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i=0; i<bulbs.size(); i++){
+        //     if(set.contains(bulbs.get(i))){
+        //         set.remove(bulbs.get(i));
+        //     } else{
+        //         set.add(bulbs.get(i));
+        //     }
+        // }
+        // List<Integer> li = new ArrayList<>(set);
+        // Collections.sort(li);
+        // return li;
     }
 }
