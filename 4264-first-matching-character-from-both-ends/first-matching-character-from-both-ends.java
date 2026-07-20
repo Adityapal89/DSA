a6f2ebf5a32dcba321cd092cd4131e534cbd661a
Aditya pal <adityapal622820@gmail.com>
2026-07-21 00:21:28 +0530

    Time: 1 ms (39.67%) | Memory: 43.4 MB (31.38%) - LeetSync

--- 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
@@ -1,21 +1,30 @@
 class Solution {
     public int firstMatchingIndex(String s) {
-        char[] arr = new char[s.length()];
-        int idx = 0;
-        int ans = -1;
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            arr[idx] = ch;
-            idx++; 
-        }
-        int n = s.length();
+        // char[] arr = new char[s.length()];
+        // int idx = 0;
+        // int ans = -1;
+        // for(int i=0; i<s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     arr[idx] = ch;
+        //     idx++; 
+        // }
+        // int n = s.length();
 
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == arr[n-i-1]){
+        // for(int i=0; i<arr.length; i++){
+        //     if(arr[i] == arr[n-i-1]){
+        //         return i;
+        //     }
+        // }
+
+        // return ans;
+
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            if(s.charAt(i) == s.charAt(n-i-1)){
                 return i;
             }
         }
 
-        return ans;
+        return -1;
     }
 }
