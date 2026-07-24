0c44f255e7b96765e84c25b161bc1e3de0012a68
Aditya pal <adityapal622820@gmail.com>
2026-07-24 13:59:26 +0530

    Time: 17 ms (16.64%) | Memory: 46.8 MB (5.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,11 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t){
-        if (s.length() != t.length()){
-            return false;
+        // if (s.length() != t.length()){
+        //     return false;
+        // }
+        // HashMap<Character, Integer> mp1 = makeFreqMap(s);
+        // HashMap<Character, Integer> mp2 = makeFreqMap(t);
+        // return mp1.equals(mp2);
+
+        HashMap<Character, Integer> mp = new HashMap<>();
+        if(s.length() != t.length()) return false;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            mp.put(ch,mp.getOrDefault(ch,0) + 1);
         }
-        HashMap<Character, Integer> mp1 = makeFreqMap(s);
-        HashMap<Character, Integer> mp2 = makeFreqMap(t);
-        return mp1.equals(mp2);
+
+        for(int i=0; i<t.length(); i++){
+            char ch = t.charAt(i);
+            mp.put(ch,mp.getOrDefault(ch,0) - 1);
+        }
+
+        for(int val : mp.values()){
+            if(val != 0) return false;
+        }
+
+        return true;
+        
     }
 
     static HashMap<Character, Integer> makeFreqMap(String str){
