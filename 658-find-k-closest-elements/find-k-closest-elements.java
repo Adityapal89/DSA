f17a1f4fbd643a354a52e6be433e20bd130cff65
Aditya pal <adityapal622820@gmail.com>
2026-08-13 16:08:29 +0530

    Time: 4 ms (96.89%) | Memory: 47.4 MB (97.71%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,28 +1,43 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (a,b)->{
-                int da = Math.abs(a-x);
-                int db = Math.abs(b-x);
-                if(da == db){
-                    return b-a;
-                }
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(
+        //     (a,b)->{
+        //         int da = Math.abs(a-x);
+        //         int db = Math.abs(b-x);
+        //         if(da == db){
+        //             return b-a;
+        //         }
 
-                return db-da;
+        //         return db-da;
+        //     }
+        // );
+
+        // for(int ele : arr){
+        //     pq.add(ele);
+        //     if(pq.size()>k) pq.poll();
+        // }
+
+        // ArrayList<Integer> ar = new ArrayList<>();
+        // while(!pq.isEmpty()){
+        //     ar.add(pq.poll());
+        // }
+
+        // Collections.sort(ar);
+        // return ar;
+
+        List<Integer> list = new ArrayList<>();
+        int right = arr.length-1;
+        int left = 0;
+        while(right-left+1 > k){
+            if(Math.abs(arr[left] - x) > Math.abs(arr[right] - x)){
+                left++;
             }
-        );
-
-        for(int ele : arr){
-            pq.add(ele);
-            if(pq.size()>k) pq.poll();
+            else
+                right--;
         }
-
-        ArrayList<Integer> ar = new ArrayList<>();
-        while(!pq.isEmpty()){
-            ar.add(pq.poll());
+        for(int i = left; i <= right; i++){
+            list.add(arr[i]);
         }
-
-        Collections.sort(ar);
-        return ar;
+        return list;
     }
 }
