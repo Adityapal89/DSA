9b44a7e0d932f2a3108aed4e3925c6c0af975636
Aditya pal <adityapal622820@gmail.com>
2026-09-16 19:18:01 +0530

    Time: 1 ms (99.28%) | Memory: 42.4 MB (78.76%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,15 +1,26 @@
 class Solution {
     public int mySqrt(int x) {
-        long i=1; 
-        long j = x/2;
+        // long i=1; 
+        // long j = x/2;
+        // if(x < 2) return x;
+        // while(i <= j){
+        //     long mid = (i+j)/2;
+        //     if(mid*mid == x) return (int)mid;
+        //     else if(mid*mid > x) j = mid - 1;
+        //     else i = mid + 1;
+        // }
+        // return (int)j;
+
+        int i=1; 
+        int j = x/2;
         if(x < 2) return x;
         while(i <= j){
-            long mid = (i+j)/2;
-            if(mid*mid == x) return (int)mid;
-            else if(mid*mid > x) j = mid - 1;
+            int mid = i + (j - i)/2;
+            if(mid == x/mid) return mid;
+            else if(mid > x/mid) j = mid - 1;
             else i = mid + 1;
         }
-        return (int)j;
+        return j;
 
         // int root = 0;
         // if(x < 2) return x;
