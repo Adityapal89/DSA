1ea85a671fa8fb8eab3a6e5e86da9fced36b169e
Aditya pal <adityapal622820@gmail.com>
2026-09-22 02:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (82.26%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -7,20 +7,41 @@ class Solution {
         // }
         // return -1;
 
+        // int i=0;
+        // int j = arr.length-1;
+        // int ans = -1;
+        // if(arr.length == 1 && arr[0] == key) return 0;
+        
+        // while(i < j){
+        //     if(arr[i] == key) return i;
+        //     if(arr[j] == key) return j;
+        //     if(key > arr[i]){
+        //         i++;
+        //     } else{
+        //         j--;
+        //     }
+        // }
+        // return ans;
+
         int i=0;
         int j = arr.length-1;
-        int ans = -1;
-        if(arr.length == 1 && arr[0] == key) return 0;
-        
-        while(i < j){
-            if(arr[i] == key) return i;
-            if(arr[j] == key) return j;
-            if(key > arr[i]){
-                i++;
-            } else{
-                j--;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(arr[mid] == key) return mid;
+            else if(arr[i] <= arr[mid]){
+                if(arr[i]<=key && key <= arr[mid]){
+                    j = mid-1;
+                } else{
+                    i = mid+1;
+                }
+            }else{
+                if(arr[mid]<key && key <= arr[j]){
+                    i = mid + 1;
+                }else{
+                    j = mid-1;
+                }
             }
         }
-        return ans;
+        return -1;
     }
 }
